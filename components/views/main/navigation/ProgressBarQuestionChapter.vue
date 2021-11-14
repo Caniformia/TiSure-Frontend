a1c2681da4d1fcec84037c8b5bc5cb5d7669985f
TakiVotoidP <895878439@qq.com>
2021-11-15 00:22:25 +0800

    Draw [Prototype].

---
+++ components/views/main/navigation/ProgressBarQuestionChapter.vue
@@ -0,0 +1,23 @@
+<template>
+  <h-stack>
+    <div class="chinese-font pr-1 opacity-60">第一章</div>
+    <div class="chinese-font">航空航天发展概况</div>
+    <spacer></spacer>
+    <div class="chinese-font pr-1 opacity-80">1</div>
+    <div class="chinese-font pr-1">/</div>
+    <div class="chinese-font">972</div>
+  </h-stack>
+</template>
+
+<script>
+import HStack from "~/components/utilities/layout/HStack";
+import Spacer from "~/components/utilities/layout/Spacer";
+export default {
+  name: "ProgressBarQuestionChapter",
+  components: {Spacer, HStack}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <stack class="p-4 h-screen flex-col md:flex-row">
+    <v-stack class="h-full overflow-auto md:flex-grow">
+      <question-navigation-module/>
+      <question-module class="flex-grow m-4 overflow-auto md:mx-0 scrollbar-hide"/>
+    </v-stack>
+    <bottom-tool-box class="md:ml-4"/>
+  </stack>
+</template>
+
+<script>
+import VStack from "~/components/utilities/layout/VStack";
+import QuestionNavigationModule from "~/components/views/main/QuestionNavigationModule";
+import Spacer from "~/components/utilities/layout/Spacer";
+import BottomToolBox from "~/components/views/main/BottomToolBox";
+import Stack from "~/components/utilities/layout/Stack";
+import QuestionModule from "~/components/views/main/QuestionModule";
+
+export default {
+  components: {QuestionModule, Stack, BottomToolBox, Spacer, QuestionNavigationModule, VStack}
+}
+</script>
