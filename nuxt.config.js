b8512e9286967be4329383037602c45fdfe3d984
TakiVotoid <895878439@qq.com>
2022-03-08 09:32:18 +0800

    Fix [Config].

---
+++ nuxt.config.js
@@ -0,0 +1,86 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'TiSure-Frontend',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''},
+      {name: 'format-detection', content: 'telephone=no'},
+      {name: 'apple-mobile-web-app-capable', content: 'yes'}
+    ],
+    link: [
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '~/assets/css/main.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '~/plugins/axios.js',
+    //{ src: '~/plugins/persistedstate.js', ssr: false}
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/auth'
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    prefix: "",
+    proxy: true,
+  },
+
+  proxy: {
+    '/api/': {
+      target: process.env.API_BASE_URL,
+      pathRewrite: {
+        '^/api/': '/'
+      }
+    }
+  },
+
+  auth: {
+    redirect: {
+      login: '/login',
+      logout: '/login',
+      callback: false,
+      home: '/'
+    },
+    strategies: {
+      local: {
+        endpoints: {
+          login: {url: '/api/auth/sign_in', method: 'post', propertyName: 'access_token'},
+          logout: {url: '/api/auth/sign_out', method: 'delete'},
+          user: false
+        },
+      }
+    },
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    transpile: [
+      'fitty'
+    ]
+  }
+}
